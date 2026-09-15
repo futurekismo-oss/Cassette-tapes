5309daf96ed93030f0d8bee811be116951da7907
futurekismo-oss <futurekismo@gmail.com>
2026-09-15 10:14:54 +0200

    feat: fixed package.nix

--- nix/package.nix
+++ nix/package.nix
@@ -7,12 +7,7 @@ rustPlatform.buildRustPackage {
   pname = "tapes";
   version = "0.1.5";
 
-  src = fetchFromGitHub {
-    owner = "futurekismo-oss";
-    repo = "Cassette-tapes";
-    rev = "bef75c5de02cb5dae78c48212ac24bce032d9bf3";
-    hash = "sha256-vhGwhU/6HTzb+CGyAB85JiHy047EwlPRUN0L+FW2PJM=";
-  };
+  src = ../.;
 
-  cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  cargoHash = "sha256-yIJoXuRdUqN4E+V/SxFM0d9d4FLqWg9W9V4d+B2PakA=";
 }
